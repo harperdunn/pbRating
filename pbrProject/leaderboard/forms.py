2c0db60612f5446a881f3551e442b0a7623ab56f
harperdunn <harperdunn@ucla.edu>
2025-05-27 13:34:42 -0700

    created anonymity options in review form

--- pbrProject/leaderboard/forms.py
+++ pbrProject/leaderboard/forms.py
@@ -2,43 +2,6 @@ from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import Review, University
 
-# class TestimonialForm(forms.ModelForm):
-#     class Meta:
-#         model = UniversityTestimonial
-#         fields = ['name', 'role', 'stars', 'quote']
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-
-#         # Make 'name' optional
-#         self.fields['name'].required = False
-#         # All other fields will remain required by default
-
-#         # Set labels and help texts
-#         self.fields['stars'].label = "Rating (1-5 stars)"
-#         self.fields['stars'].help_text = "Please rate this university's plant-based options and related progress from 1 to 5 stars"
-        
-#         # Custom widget attributes
-#         self.fields['name'].widget.attrs.update({
-#             'class': 'form-control',
-#             'placeholder': 'Your Name (Optional)'
-#         })
-#         self.fields['role'].widget.attrs.update({
-#             'class': 'form-control',
-#             'placeholder': 'Your Role (e.g., Current Student, Alumni) (Required)'
-#         })
-#         self.fields['stars'].widget.attrs.update({
-#             'class': 'form-control',
-#             'min': 1,
-#             'max': 5,
-#             'type': 'number'
-#         })
-#         self.fields['quote'].widget.attrs.update({
-#             'class': 'form-control',
-#             'placeholder': 'Share your experience (Required)',
-#             'rows': 4
-#         })
-
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
